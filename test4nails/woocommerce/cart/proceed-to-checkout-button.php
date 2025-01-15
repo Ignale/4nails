38f285cb7c42b03d5e6b1b12097205a95af3886a
Alexey Ignatev <ignat.lex@gmail.com>
2025-01-15 08:26:21 +0300

    deleting different warehouse dependencies

--- test4nails/woocommerce/cart/proceed-to-checkout-button.php
+++ test4nails/woocommerce/cart/proceed-to-checkout-button.php
@@ -1,26 +1,23 @@
 <?php
 $loggedUser = is_user_logged_in();
 
-$diffWarehouses = if_different_warehouses(WC()->cart);
+// $diffWarehouses = if_different_warehouses(WC()->cart);
 
 $overweight = if_overweight(WC()->cart);
 
 $free_delivery = if_free_delivery(WC()->cart);
 
-//Ignore overweight in cart if free delivery and diffWarehouses are checked in any product  
-$ignore_overweight = $free_delivery['free_delivery'] && $diffWarehouses['different'];
-
 // Checking that in the basket all goods are only only_free_delivery
 $only_free_delivery = $free_delivery['free_delivery'] && $free_delivery['only_free_delivery'];
 
 /* If cart have overweight or if cart items have different warehouses or if user isnt logged in and we are not ignoring overwheight*/
-$haveTrouble = !$loggedUser || ($overweight && !$ignore_overweight) ? true : false;
+$haveTrouble = !$loggedUser || ($overweight && !$free_delivery['free_delivery']) ? true : false;
 
 /* If have overweight and we are not ignoring overwheight*/
 $changePopupLink = 0;
-if (!$overweight && !$ignore_overweight) {
+if (!$overweight && !$free_delivery['free_delivery']) {
   $changePopupLink = 'login-error';
-} elseif ($overweight && !$ignore_overweight) {
+} elseif ($overweight && !$free_delivery['free_delivery']) {
   $changePopupLink = 'overweight';
 }
 $href = $haveTrouble ? '' : esc_url(wc_get_checkout_url());
@@ -35,20 +32,20 @@ $href = $haveTrouble ? '' : esc_url(wc_get_checkout_url());
 */
 $id = '';
 /* If user logged in, and dont have overweight and we are not ignoring overwheight*/
-if (!$only_free_delivery && $diffWarehouses['different'] && ($free_delivery['free_delivery'] ?? false)) { // проверка на наличие товара с свободной доставкой
+if (!$only_free_delivery && $free_delivery['free_delivery']) { // проверка на наличие товара с свободной доставкой
   $id = 'overweight';
-} elseif ($loggedUser && !($overweight && !$ignore_overweight)) {
+} elseif ($loggedUser && !($overweight && !$free_delivery['free_delivery'])) {
   $id = 'goToCheckoutBtn';
 } elseif (!$loggedUser) { // if user is not logged in but has no troubles
   $id = 'login-btn';
-} elseif (!$overweight && !$ignore_overweight) { // if logged in but has items from different warehouses
+} elseif (!$overweight && !$free_delivery['free_delivery']) { // if logged in but has items from different warehouses
   $id = 'login-error';
-} elseif ($overweight && !$ignore_overweight) { // if logged in but has overweight items
+} elseif ($overweight && !$free_delivery['free_delivery']) { // if logged in but has overweight items
   $id = 'overweight';
 }
 ?>
 <a
-href="<?= $w ?>"
+href="<?php echo esc_url(wc_get_checkout_url()); ?>"
 class="red-btn checkout-button button btn-cyan w-100 alt wc-forward"
 id="<?= $id ?>"
 >
@@ -56,15 +53,14 @@ id="<?= $id ?>"
   ?>
 </a>
 
-
 <?php
 if (!$loggedUser) {
-  get_template_part('widgets/cart/cart', 'login', ['have_trouble' => $diffWarehouses['different'] || $overweight ? $changePopupLink : 0]);
+  get_template_part('widgets/cart/cart', 'login', ['have_trouble' => $free_delivery['free_delivery'] || $overweight ? $changePopupLink : 0]);
 }
-if ($diffWarehouses['different'] && (!$only_free_delivery && $free_delivery['free_delivery'] ?? false)) {
-  get_template_part('widgets/cart/cart', 'notAllow', ['products_id' => $diffWarehouses['ids']]);
+if (!$only_free_delivery && $free_delivery['free_delivery']) {
+  get_template_part('widgets/cart/cart', 'notAllow', ['products_id' => $free_delivery['ids']]);
 }
-if ($overweight && !$ignore_overweight) {
+if ($overweight && !$free_delivery['free_delivery']) {
   get_template_part('widgets/cart/cart', 'overweight', ['cart' => WC()->cart]);
 }
 ?>
