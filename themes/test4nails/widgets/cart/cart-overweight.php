1e349c9620ccd119389b29dfbf29590329e3f3dc
Alexey Ignatev <ignat.lex@gmail.com>
2025-06-26 17:08:52 +0300

    fix

--- themes/test4nails/widgets/cart/cart-overweight.php
+++ themes/test4nails/widgets/cart/cart-overweight.php
@@ -1,20 +1,13 @@
-<?php
-$cart = WC()->cart;
-$weight = $args['weight'];
-$quantity = $args['qty'];
-$button_text = $args['button_text'];
-$is_cart = $args['is_cart'];
-$show_cart_button = $args['show_cart_button'];
-$weight = $weight ?: get_cart_weight($cart);
-?>
+<?php $cart = WC()->cart; ?>
+
 
 <div
 style="display: none; width: 500px;"
 class="modal fade"
-data-message-type = "overweight"
 id="overweight-massage"
 tabindex="-1"
 >
+
   <div
   class="modal-dialog modal-dialog-centered"
   role="document"
@@ -35,17 +28,12 @@ tabindex="-1"
             alt="<?php _e('Remove', 'ti-woocommerce-wishlist') ?>"
             >
           </div>
-          <div data-max-weight = '30' data-weight = '<?= $weight?>' class="modal-error__header">
-            <?= $weight > 30 ? sprintf('The weight of items in your cart exceeds the allowed limit for a single shipment!
-              Maximum weight per order: <b>30 lbs</b>
-              Total weight of your order: <b>%s lbs</b> .
-              To complete your order:<br/>
-              1. Remove some items from your cart to reduce the weight.<br/>
-              2. Place a second order for the remaining items.', $weight) :
-              sprintf('Thank you! Right now the weight of the item is <b>%s lbs</b> . this is the allowable weight for one order (30 lbs)', $weight)
-              ?>
-              
-
+          <div class="modal-error__header">
+            <?= __('Unfortunately, due to the size of the items in your cart, we cannot deliver all of the products in one shipment. Please place 2 separate orders for these items. To do this, return to the cart and delete some items or reduce the quantity.', '4nails') ?>
+            <div class="modal-error__header-details">
+              <p><?= __('Total weight of cart items:', '4nails'); ?> <b><?= get_cart_weight($cart) ?> lbs</b>
+                <?= __(' (25 lbs maximum)', '4nails'); ?></p>
+            </div>
           </div>
           <div class="modal-error__wrap">
 
@@ -53,66 +41,24 @@ tabindex="-1"
 
               <?php
               $temp = 1;
-
               foreach ($cart->get_cart() as $cart_item_key => $cart_item):
 
                 $product = $cart_item['data'];
-                $max_value = $product->get_stock_quantity();
-                $value = $cart_item['quantity'];
-                $is_avialble = $value >= $max_value ? false : true;
-
                 if (!get_field('attached_product', $product->get_id())):
                   ?>
 
                   <div
                   <?= $temp == 1 ? "id='firstItem'" : ""; ?>
                   class="modal-error__item"
-                   data-product-id = <?= $product->get_id() ?> 
-                   data-item-key = '<?= $cart_item_key ?>'
-                   data-qty = '<?= $cart_item['quantity'] ?>'
                   >
 
                     <div class="modal-error__item-img"><?= $product->get_image() ?></div>
 
                     <div class="modal-error__item-text"><a
                       href="<?= get_permalink($product->get_id()) ?>"><?= $product->get_name() ?></a>
-                      <p><b><?= sprintf('Weight: %s lbs.', $product->get_weight()) ?></b></p>
+                      <p><b><?= sprintf('Weight of one: %s lbs.', $product->get_weight()) ?></b></p>
                       <p><b><?= sprintf('Count: %s', $cart_item['quantity']) ?></b></p>
                     </div>
-                  <?php if (!$is_cart) { ?>
-                    <div class="modal-error__item-change">
-                      <?php if ($cart_item['quantity'] > 1) { ?>
-                        <button id = 'decrease_qty' 
-                        class="red-btn minus"
-                        data-action="updateProduct"
-                        >
-                        <img class="cart_action_img" src="<?= path() ?>assets/img/icons/minus.svg"
-                        alt="rolling"
-                        >
-                      <img style="display: none" class="cart_action_img__loading" src="<?= path() ?>assets/img/icons/rolling.svg"
-                      alt="rolling"
-                      ></button>
-                    <?php } ?>
-                    <button id = 'increase_qty' 
-                    class="red-btn plus <?= !$is_avialble ?  esc_attr( 'disabled' ) : ''?>"
-                     data-action="updateProduct"
-                    >
-                    <img class="cart_action_img" src="<?= path() ?>assets/img/icons/plus.svg"
-                    alt="rolling"
-                    >
-                  <img style="display: none" class="cart_action_img__loading" src="<?= path() ?>assets/img/icons/rolling.svg"
-                  alt="rolling"
-                  ></button>
-                    </div>
-                    <div class="modal-error__item-remove">
-                      <button id = 'remove_from_cart' data-action="removeFromCart" class="red-btn remove_btn">
-                      <img class="cart_action_img" src="<?= path() ?>assets/img/icons/trash.svg" alt="trash">
-                      <img style="display: none" class="cart_action_img__loading" src="<?= path() ?>assets/img/icons/rolling.svg" alt="rolling">
-                    
-                    </button>
-                    </div>
-                    <?php } ?>
-                    
 
 
                   </div>
@@ -122,23 +68,28 @@ tabindex="-1"
                 endif;
               endforeach;
               ?>
+              <div class="show-more">
+                <div class="show-more__btn">
+                  <div class="show-more__text">
+                    <div id="showMore"><?= __('Show more', '4nails'); ?></div>
+                    <div id="hideMore"><?= __('Hide', '4nails'); ?></div>
+                  </div>
+                  <div class="show-more-arrow show"></div>
+                </div>
+              </div>
             </div>
 
 
-            <div class="add-product__btn">
-              <?php if ($show_cart_button) { ?>
-              
+            <div class="modal-error__buttons">
 
-            <a href="<?php cart_url() ?>"
-            class="view-cart red-btn"
-            ><?= __('View Cart', '4nails') ?></a>
 
-            <?php } ?>
-            
 
-            <div class="continue modal-error__go-shop red-btn" 
-            id="backToCart"
-            onclick="Fancybox.close()"><?= __($button_text ?: 'back to cart', '4nails'); ?></div>
+              <div
+              class="modal-error__go-shop red-btn"
+              id="backToCart"
+              ><?= __('Return to the cart', '4nails'); ?></div>
+
+
 
             </div>
 
